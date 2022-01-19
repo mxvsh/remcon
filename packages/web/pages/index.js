7b158bb6b5e4e32c1e3c11dbc1f702c316b3e2b2
xencodes <xencodes@icloud.com>
2022-01-19 14:23:32 +0530

    init

---
+++ packages/web/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>remcon</title>
+				<meta name='description' content='Remote console.log' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+		</div>
+	);
+}
